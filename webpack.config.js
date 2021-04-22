6f00cb0343ead15c5242fbb3bffd19fe6387c1a0
oscarparedez <oaparedez17@gmail.com>
2021-04-21 18:00:58 -0600

    Calculator React (Tests Included)

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    mode: 'production',
-    devServer: {
-        contentBase: 'dist',
-        port: 3000,
-    },
-    module: {
-        rules: [
-            {
-                test: /.(jsx?)$/,
-                use: ['babel-loader'],
-              },
-            {
-                test: /.css$/,
-                use: ['style-loader', 'css-loader'],
-              },
-        ]
-    }  
+  mode: 'production',
+  devServer: {
+    contentBase: 'dist',
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /.(jsx?)$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  }  
 }
